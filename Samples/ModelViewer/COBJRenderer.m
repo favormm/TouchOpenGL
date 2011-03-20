43625d85fb25b9e0553f6ac401882cd79ced501f
Jonathan Wight <jwight@mac.com>
2011-03-20 10:39:11 -0700

    Attempting to get complex lighting shaders in place.

--- Samples/ModelViewer/COBJRenderer.m
+++ Samples/ModelViewer/COBJRenderer.m
@@ -20,10 +20,26 @@
 #import "CMaterial.h"
 #import "CRenderer_Extensions.h"
 
+struct directional_light {
+	Vector3 direction;
+	Vector3 halfplane;
+	Color4f ambient_color;
+	Color4f diffuse_color;
+	Color4f specular_color;
+};
+
+struct material_properties {
+	Color4f ambient_color;
+	Color4f diffuse_color;
+	Color4f specular_color;
+	GLfloat specular_exponent;
+};
+
 @interface COBJRenderer ()
 @property (readwrite, nonatomic, retain) NSArray *meshes;
 @property (readwrite, nonatomic, retain) CProgram *flatProgram;
 @property (readwrite, nonatomic, retain) CProgram *textureProgram;
+@property (readwrite, nonatomic, retain) CProgram *lightingProgram;
 @property (readwrite, nonatomic, retain) CTexture *defaultTexture;
 @end
 
@@ -32,6 +48,7 @@
 @synthesize meshes;
 @synthesize flatProgram;
 @synthesize textureProgram;
+@synthesize lightingProgram;
 @synthesize defaultTexture;
 
 - (id)init
@@ -40,9 +57,13 @@
 		{
         CMeshLoader *theLoader = [[[CMeshLoader alloc] init] autorelease];
         self.meshes = [theLoader loadMeshesFromFile:@"Skull"];
+                
         
-        self.flatProgram = [[[CProgram alloc] initWithName:@"Flat2"] autorelease];
-        self.textureProgram = [[[CProgram alloc] initWithName:@"SimpleTexture"] autorelease];
+        self.flatProgram = [[[CProgram alloc] initWithName:@"Flat2" attributeNames:[NSArray arrayWithObjects:@"a_position", @"a_normal", NULL] uniformNames:[NSArray arrayWithObjects:@"u_mvpMatrix", @"u_diffuse_color", @"u_color", NULL]] autorelease];
+
+        self.textureProgram = [[[CProgram alloc] initWithName:@"SimpleTexture" attributeNames:[NSArray arrayWithObjects:@"a_position", @"a_texCoord", NULL] uniformNames:[NSArray arrayWithObjects:@"u_mvpMatrix", NULL]] autorelease];
+
+        self.lightingProgram = [[[CProgram alloc] initWithName:@"Lighting" attributeNames:[NSArray arrayWithObjects:@"a_position", @"a_normal", NULL] uniformNames:[NSArray arrayWithObjects:@"u_mvpMatrix", @"u_modelViewMatrix" /*, @"u_material", @"u_light"*/, NULL]] autorelease];
 		}
 	return(self);
 	}
@@ -52,9 +73,13 @@
     AssertOpenGLNoError_();
 
 
-    const Matrix4 theTransform = Matrix4Scale(inTransform, 0.01, 0.01, 0.01);
+    const Matrix4 theTransform = Matrix4Scale(inTransform, 0.05, 0.05, 0.05);
 
     [self drawAxes:theTransform];
+
+//    glFrontFace(GL_CCW);
+//    glCullFace(GL_BACK);
+//    glEnable(GL_CULL_FACE);
 
 
     for (CMesh *theMesh in self.meshes)
@@ -74,12 +99,16 @@
         glUseProgram(theProgram.name);
         
         // Update position attribute
-        GLuint theVertexAttributeIndex = [theProgram attributeIndexForName:@"a_vertex"];        
-        [theMesh.positions use:theVertexAttributeIndex];
-        glEnableVertexAttribArray(theVertexAttributeIndex);
+        GLuint thePositionsAttributeIndex = [theProgram attributeIndexForName:@"a_position"];        
+        [theMesh.positions use:thePositionsAttributeIndex];
+        glEnableVertexAttribArray(thePositionsAttributeIndex);
+
+        GLuint theNormalsAttributeIndex = [theProgram attributeIndexForName:@"a_normal"];        
+        [theMesh.normals use:theNormalsAttributeIndex];
+        glEnableVertexAttribArray(theNormalsAttributeIndex);
 
         // Update transform uniform
-        GLuint theTransformUniformIndex = [theProgram uniformIndexForName:@"u_transform"];
+        GLuint theTransformUniformIndex = [theProgram uniformIndexForName:@"u_mvpMatrix"];
         glUniformMatrix4fv(theTransformUniformIndex, 1, NO, &theMeshTransform.m00);
 
         if (theProgram == self.textureProgram)
@@ -88,23 +117,32 @@
             
             glBindTexture(GL_TEXTURE_2D, theTexture.name);
 
-            GLuint theTextureAttributeIndex = [theProgram attributeIndexForName:@"a_texture"];        
+            GLuint theTextureAttributeIndex = [theProgram attributeIndexForName:@"a_texCoord"];        
             [theMesh.texCoords use:theTextureAttributeIndex];
             glEnableVertexAttribArray(theTextureAttributeIndex);
             }
-        else
+        else if (theProgram == self.flatProgram)
             {
-            // Update color uniform
-            Color4f theDiffuseColor = theMesh.material.diffuseColor;
-            GLuint theDiffuseColorUniformIndex = [theProgram uniformIndexForName:@"u_diffuse_color"];
-            glUniform4fv(theDiffuseColorUniformIndex, 1, &theDiffuseColor.r);
-
-            // Update color uniform
-            Color4f theAmbientColor = theMesh.material.ambientColor;
-            GLuint theAmbientColorUniformIndex = [theProgram uniformIndexForName:@"u_ambient_color"];
-            glUniform4fv(theAmbientColorUniformIndex, 1, &theAmbientColor.r);
+            Color4f theColor = [UIColor redColor].color4f;
+            GLuint theColorUniformIndex = [theProgram uniformIndexForName:@"u_color"];
+            glUniform4fv(theColorUniformIndex, 1, &theColor.r);
             }
+        else if (theProgram == self.lightingProgram)
+            {
+            AssertOpenGLNoError_();
+            
+            GLfloat theMat3[] = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+            
+            GLuint theModelMatrixUniformIndex = [theProgram uniformIndexForName:@"u_modelViewMatrix"];
+            glUniformMatrix3fv(theModelMatrixUniformIndex, 1, NO, theMat3);
 
+            AssertOpenGLNoError_();
+
+            
+//            Color4f theColor = [UIColor redColor].color4f;
+//            GLuint theColorUniformIndex = [theProgram uniformIndexForName:@"u_color"];
+//            glUniform4fv(theColorUniformIndex, 1, &theColor.r);
+            }
 
 
         // Validate program before drawing. This is a good check, but only really necessary in a debug build. DEBUG macro must be defined in your debug configurations if that's not already the case.
