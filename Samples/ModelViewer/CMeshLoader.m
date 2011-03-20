c1d78e93ebb0a1b97a7773e1d848783b5dfd3991
Jonathan Wight <jwight@mac.com>
2011-03-20 10:38:32 -0700

    Multiple meshes per mesh file.

--- Samples/ModelViewer/CMeshLoader.m
+++ Samples/ModelViewer/CMeshLoader.m
@@ -17,6 +17,7 @@
 #import "CVertexBufferReference.h"
 #import "OpenGLTypes.h"
 #import "CMaterial.h"
+#import "CVertexBuffer_StringExtensions.h"
 
 @implementation CMeshLoader
 
@@ -93,7 +94,40 @@
     for (NSDictionary *theMeshDictionary in theMeshDictionaries)
         {
         NSString *theMaterialName = [theMeshDictionary objectForKey:@"material"];
-              
+
+        NSString *theVerticesString = [theMeshDictionary objectForKey:@"vertices"];
+        if (theVerticesString != NULL)
+            {
+            CMesh *theMesh = [[[CMesh alloc] init] autorelease];
+            theMesh.center = theCenter;
+			theMesh.material = [theMaterials objectForKey:theMaterialName];
+
+            CVertexBuffer *theVertexBuffer = [CVertexBuffer vertexBufferWithString:theVerticesString target:GL_ARRAY_BUFFER usage:GL_STATIC_DRAW];
+			size_t theRowCount = theVertexBuffer.data.length / sizeof(Vector3);
+			theMesh.positions = [[[CVertexBufferReference alloc] initWithVertexBuffer:theVertexBuffer rowSize:sizeof(Vector3) rowCount:theRowCount size:3 type:GL_FLOAT normalized:NO stride:sizeof(Vector3) offset:0] autorelease];
+
+            NSString *theVerticesString = [theMeshDictionary objectForKey:@"texCoords"];
+            if (theVerticesString != NULL)
+                {
+                CVertexBuffer *theVertexBuffer = [CVertexBuffer vertexBufferWithString:theVerticesString target:GL_ARRAY_BUFFER usage:GL_STATIC_DRAW];
+                size_t theRowCount = theVertexBuffer.data.length / sizeof(Vector2);
+                theMesh.texCoords = [[[CVertexBufferReference alloc] initWithVertexBuffer:theVertexBuffer rowSize:sizeof(Vector2) rowCount:theRowCount size:2 type:GL_FLOAT normalized:NO stride:sizeof(Vector2) offset:0] autorelease];
+                }
+
+            theVerticesString = [theMeshDictionary objectForKey:@"normals"];
+            if (theVerticesString != NULL)
+                {
+                CVertexBuffer *theVertexBuffer = [CVertexBuffer vertexBufferWithString:theVerticesString target:GL_ARRAY_BUFFER usage:GL_STATIC_DRAW];
+                size_t theRowCount = theVertexBuffer.data.length / sizeof(Vector3);
+                theMesh.normals = [[[CVertexBufferReference alloc] initWithVertexBuffer:theVertexBuffer rowSize:sizeof(Vector3) rowCount:theRowCount size:2 type:GL_FLOAT normalized:NO stride:sizeof(Vector3) offset:0] autorelease];
+                }
+
+            
+			[theMeshes addObject:theMesh];
+            }
+
+
+
         NSArray *theVBONames = [theMeshDictionary objectForKey:@"VBOs"];
 		for (NSString *theVBOName in theVBONames)
 			{
