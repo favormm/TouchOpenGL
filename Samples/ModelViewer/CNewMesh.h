803bbd948722fce2ac5120e8ded0dc028aad4654
Jonathan Wight <jwight@mac.com>
2011-03-23 11:43:36 -0700

    Adding more asserts "for your safety(tm)"

---
+++ Samples/ModelViewer/CNewMesh.h
@@ -0,0 +1,16 @@
+//
+//  CNewMesh.h
+//  ModelViewer
+//
+//  Created by Jonathan Wight on 03/22/11.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CNewMesh : NSObject {
+    
+}
+
+@end
