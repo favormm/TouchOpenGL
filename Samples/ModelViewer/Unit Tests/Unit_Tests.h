267a8d4b04247ba5dc3bf78f40bcf419319ec56f
Jonathan Wight <jwight@mac.com>
2011-03-25 12:52:07 -0700

    Added (empty!) unit tests.

---
+++ Samples/ModelViewer/Unit Tests/Unit_Tests.h
@@ -0,0 +1,17 @@
+//
+//  Unit_Tests.h
+//  Unit Tests
+//
+//  Created by Jonathan Wight on 03/25/11.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Unit_Tests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ Samples/ModelViewer/Unit Tests/Unit_Tests.m
@@ -0,0 +1,33 @@
+//
+//  Unit_Tests.m
+//  Unit Tests
+//
+//  Created by Jonathan Wight on 03/25/11.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
+//
+
+#import "Unit_Tests.h"
+
+
+@implementation Unit_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    STFail(@"Unit tests are not implemented yet in Unit Tests");
+}
+
+@end
