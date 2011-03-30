b579386665b0da62b6fc54adc2c6b7cd94362c7b
Jonathan Wight <jwight@mac.com>
2011-03-30 11:49:53 -0700

    Fixed initialize warnings. Turned floats into GLfloats. Made consts const. Using tgmath.h for math

--- Source/OpenGL/Matrix.m
+++ Source/OpenGL/Matrix.m
@@ -9,15 +9,20 @@
 
 #import "Matrix.h"
  
+#include <tgmath.h>
+
 #import "OpenGLTypes.h"
 
 // Matrix4 code based on code from http://sunflow.sourceforge.net/
 
 const Matrix4 Matrix4Identity = {
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0 };
+    .m = {
+        { 1.0, 0.0, 0.0, 0.0 },
+        { 0.0, 1.0, 0.0, 0.0 },
+        { 0.0, 0.0, 1.0, 0.0 },
+        { 0.0, 0.0, 0.0, 1.0 },
+        }
+    };
 
 BOOL Matrix4IsIdentity(Matrix4 t)
     {
@@ -32,46 +37,52 @@ BOOL Matrix4EqualToTransform(Matrix4 a, Matrix4 b)
 Matrix4 Matrix4MakeTranslation(GLfloat tx, GLfloat ty, GLfloat tz)
     {
     const Matrix4 theMatrix = {
-        1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0,
-        0.0, 0.0, 1.0, 0.0,
-        tx, ty, tz, 1.0 };
+        .m = {
+            { 1.0, 0.0, 0.0, 0.0 },
+            { 0.0, 1.0, 0.0, 0.0 },
+            { 0.0, 0.0, 1.0, 0.0 },
+            { tx,  ty,  tz,  1.0 },
+            }
+        };
     return(theMatrix);
     }
     
 Matrix4 Matrix4MakeScale(GLfloat sx, GLfloat sy, GLfloat sz)
     {
     const Matrix4 theMatrix = {
-        sx, 0.0, 0.0, 0.0,
-        0.0, sy, 0.0, 0.0,
-        0.0, 0.0, sz, 0.0,
-        0.0, 0.0, 0.0, 1.0 };
+        .m = {
+            { sx, 0.0, 0.0, 0.0 },
+            { 0.0, sy, 0.0, 0.0 },
+            { 0.0, 0.0, sz, 0.0 },
+            { 0.0, 0.0, 0.0, 1.0 },
+            }
+        };
     return(theMatrix);
     }
     
 Matrix4 Matrix4MakeRotation(GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
     {
     Matrix4 m = {};
-    float invLen = 1.0 / sqrtf(x * x + y * y + z * z);
+    const GLfloat invLen = 1.0 / sqrt(x * x + y * y + z * z);
     x *= invLen;
     y *= invLen;
     z *= invLen;
-    float s = sinf(angle);
-    float c = cosf(angle);
-    float t = 1.0 - c;
+    const GLfloat s = sin(angle);
+    const GLfloat c = cos(angle);
+    const GLfloat t = 1.0 - c;
     m.m[0][0] = t * x * x + c;
     m.m[1][1] = t * y * y + c;
     m.m[2][2] = t * z * z + c;
-    float txy = t * x * y;
-    float sz = s * z;
+    const GLfloat txy = t * x * y;
+    const GLfloat sz = s * z;
     m.m[0][1] = txy - sz;
     m.m[1][0] = txy + sz;
-    float txz = t * x * z;
-    float sy = s * y;
+    const GLfloat txz = t * x * z;
+    const GLfloat sy = s * y;
     m.m[0][2] = txz + sy;
     m.m[2][0] = txz - sy;
-    float tyz = t * y * z;
-    float sx = s * x;
+    const GLfloat tyz = t * y * z;
+    const GLfloat sx = s * x;
     m.m[1][2] = tyz - sx;
     m.m[2][1] = tyz + sx;
     m.m[3][3] = 1.0;
@@ -95,66 +106,83 @@ Matrix4 Matrix4Rotate(Matrix4 t, GLfloat angle, GLfloat x, GLfloat y, GLfloat z)
     
 Matrix4 Matrix4Concat(Matrix4 a, Matrix4 m)
     {
-    Matrix4 theMatrix = {
-        a.m[0][0] * m.m[0][0] + a.m[0][1] * m.m[1][0] + a.m[0][2] * m.m[2][0] + a.m[0][3] * m.m[3][0],
-        a.m[0][0] * m.m[0][1] + a.m[0][1] * m.m[1][1] + a.m[0][2] * m.m[2][1] + a.m[0][3] * m.m[3][1],
-        a.m[0][0] * m.m[0][2] + a.m[0][1] * m.m[1][2] + a.m[0][2] * m.m[2][2] + a.m[0][3] * m.m[3][2],
-        a.m[0][0] * m.m[0][3] + a.m[0][1] * m.m[1][3] + a.m[0][2] * m.m[2][3] + a.m[0][3] * m.m[3][3],
-
-        a.m[1][0] * m.m[0][0] + a.m[1][1] * m.m[1][0] + a.m[1][2] * m.m[2][0] + a.m[1][3] * m.m[3][0],
-        a.m[1][0] * m.m[0][1] + a.m[1][1] * m.m[1][1] + a.m[1][2] * m.m[2][1] + a.m[1][3] * m.m[3][1],
-        a.m[1][0] * m.m[0][2] + a.m[1][1] * m.m[1][2] + a.m[1][2] * m.m[2][2] + a.m[1][3] * m.m[3][2],
-        a.m[1][0] * m.m[0][3] + a.m[1][1] * m.m[1][3] + a.m[1][2] * m.m[2][3] + a.m[1][3] * m.m[3][3],
-
-        a.m[2][0] * m.m[0][0] + a.m[2][1] * m.m[1][0] + a.m[2][2] * m.m[2][0] + a.m[2][3] * m.m[3][0],
-        a.m[2][0] * m.m[0][1] + a.m[2][1] * m.m[1][1] + a.m[2][2] * m.m[2][1] + a.m[2][3] * m.m[3][1],
-        a.m[2][0] * m.m[0][2] + a.m[2][1] * m.m[1][2] + a.m[2][2] * m.m[2][2] + a.m[2][3] * m.m[3][2],
-        a.m[2][0] * m.m[0][3] + a.m[2][1] * m.m[1][3] + a.m[2][2] * m.m[2][3] + a.m[2][3] * m.m[3][3],
-
-        a.m[3][0] * m.m[0][0] + a.m[3][1] * m.m[1][0] + a.m[3][2] * m.m[2][0] + a.m[3][3] * m.m[3][0],
-        a.m[3][0] * m.m[0][1] + a.m[3][1] * m.m[1][1] + a.m[3][2] * m.m[2][1] + a.m[3][3] * m.m[3][1],
-        a.m[3][0] * m.m[0][2] + a.m[3][1] * m.m[1][2] + a.m[3][2] * m.m[2][2] + a.m[3][3] * m.m[3][2],
-        a.m[3][0] * m.m[0][3] + a.m[3][1] * m.m[1][3] + a.m[3][2] * m.m[2][3] + a.m[3][3] * m.m[3][3],
-        };
+    Matrix4 theMatrix = { 
+        .m = { 
+            {
+            a.m[0][0] * m.m[0][0] + a.m[0][1] * m.m[1][0] + a.m[0][2] * m.m[2][0] + a.m[0][3] * m.m[3][0],
+            a.m[0][0] * m.m[0][1] + a.m[0][1] * m.m[1][1] + a.m[0][2] * m.m[2][1] + a.m[0][3] * m.m[3][1],
+            a.m[0][0] * m.m[0][2] + a.m[0][1] * m.m[1][2] + a.m[0][2] * m.m[2][2] + a.m[0][3] * m.m[3][2],
+            a.m[0][0] * m.m[0][3] + a.m[0][1] * m.m[1][3] + a.m[0][2] * m.m[2][3] + a.m[0][3] * m.m[3][3],
+            },
+            {
+            a.m[1][0] * m.m[0][0] + a.m[1][1] * m.m[1][0] + a.m[1][2] * m.m[2][0] + a.m[1][3] * m.m[3][0],
+            a.m[1][0] * m.m[0][1] + a.m[1][1] * m.m[1][1] + a.m[1][2] * m.m[2][1] + a.m[1][3] * m.m[3][1],
+            a.m[1][0] * m.m[0][2] + a.m[1][1] * m.m[1][2] + a.m[1][2] * m.m[2][2] + a.m[1][3] * m.m[3][2],
+            a.m[1][0] * m.m[0][3] + a.m[1][1] * m.m[1][3] + a.m[1][2] * m.m[2][3] + a.m[1][3] * m.m[3][3],
+            },
+            {
+            a.m[2][0] * m.m[0][0] + a.m[2][1] * m.m[1][0] + a.m[2][2] * m.m[2][0] + a.m[2][3] * m.m[3][0],
+            a.m[2][0] * m.m[0][1] + a.m[2][1] * m.m[1][1] + a.m[2][2] * m.m[2][1] + a.m[2][3] * m.m[3][1],
+            a.m[2][0] * m.m[0][2] + a.m[2][1] * m.m[1][2] + a.m[2][2] * m.m[2][2] + a.m[2][3] * m.m[3][2],
+            a.m[2][0] * m.m[0][3] + a.m[2][1] * m.m[1][3] + a.m[2][2] * m.m[2][3] + a.m[2][3] * m.m[3][3],
+            },
+            {
+            a.m[3][0] * m.m[0][0] + a.m[3][1] * m.m[1][0] + a.m[3][2] * m.m[2][0] + a.m[3][3] * m.m[3][0],
+            a.m[3][0] * m.m[0][1] + a.m[3][1] * m.m[1][1] + a.m[3][2] * m.m[2][1] + a.m[3][3] * m.m[3][1],
+            a.m[3][0] * m.m[0][2] + a.m[3][1] * m.m[1][2] + a.m[3][2] * m.m[2][2] + a.m[3][3] * m.m[3][2],
+            a.m[3][0] * m.m[0][3] + a.m[3][1] * m.m[1][3] + a.m[3][2] * m.m[2][3] + a.m[3][3] * m.m[3][3],
+            },
+        }
+    };
     return(theMatrix);
     }
     
 Matrix4 Matrix4Invert(Matrix4 t)
     {
-    float A0 = t.m[0][0] * t.m[1][1] - t.m[0][1] * t.m[1][0];
-    float A1 = t.m[0][0] * t.m[1][2] - t.m[0][2] * t.m[1][0];
-    float A2 = t.m[0][0] * t.m[1][3] - t.m[0][3] * t.m[1][0];
-    float A3 = t.m[0][1] * t.m[1][2] - t.m[0][2] * t.m[1][1];
-    float A4 = t.m[0][1] * t.m[1][3] - t.m[0][3] * t.m[1][1];
-    float A5 = t.m[0][2] * t.m[1][3] - t.m[0][3] * t.m[1][2];
+    const GLfloat A0 = t.m[0][0] * t.m[1][1] - t.m[0][1] * t.m[1][0];
+    const GLfloat A1 = t.m[0][0] * t.m[1][2] - t.m[0][2] * t.m[1][0];
+    const GLfloat A2 = t.m[0][0] * t.m[1][3] - t.m[0][3] * t.m[1][0];
+    const GLfloat A3 = t.m[0][1] * t.m[1][2] - t.m[0][2] * t.m[1][1];
+    const GLfloat A4 = t.m[0][1] * t.m[1][3] - t.m[0][3] * t.m[1][1];
+    const GLfloat A5 = t.m[0][2] * t.m[1][3] - t.m[0][3] * t.m[1][2];
 
-    float B0 = t.m[2][0] * t.m[3][1] - t.m[2][1] * t.m[3][0];
-    float B1 = t.m[2][0] * t.m[3][2] - t.m[2][2] * t.m[3][0];
-    float B2 = t.m[2][0] * t.m[3][3] - t.m[2][3] * t.m[3][0];
-    float B3 = t.m[2][1] * t.m[3][2] - t.m[2][2] * t.m[3][1];
-    float B4 = t.m[2][1] * t.m[3][3] - t.m[2][3] * t.m[3][1];
-    float B5 = t.m[2][2] * t.m[3][3] - t.m[2][3] * t.m[3][2];
+    const GLfloat B0 = t.m[2][0] * t.m[3][1] - t.m[2][1] * t.m[3][0];
+    const GLfloat B1 = t.m[2][0] * t.m[3][2] - t.m[2][2] * t.m[3][0];
+    const GLfloat B2 = t.m[2][0] * t.m[3][3] - t.m[2][3] * t.m[3][0];
+    const GLfloat B3 = t.m[2][1] * t.m[3][2] - t.m[2][2] * t.m[3][1];
+    const GLfloat B4 = t.m[2][1] * t.m[3][3] - t.m[2][3] * t.m[3][1];
+    const GLfloat B5 = t.m[2][2] * t.m[3][3] - t.m[2][3] * t.m[3][2];
 
-    float det = A0 * B5 - A1 * B4 + A2 * B3 + A3 * B2 - A4 * B1 + A5 * B0;
+    const GLfloat det = A0 * B5 - A1 * B4 + A2 * B3 + A3 * B2 - A4 * B1 + A5 * B0;
     NSCAssert(fabs(det) >= 1e-12f, @"Not invertable");
-    float invDet = 1.0 / det;
+    const GLfloat invDet = 1.0 / det;
     Matrix4 m = {
-        (+t.m[1][1] * B5 - t.m[1][2] * B4 + t.m[1][3] * B3) * invDet,
-        (-t.m[1][0] * B5 + t.m[1][2] * B2 - t.m[1][3] * B1) * invDet,
-        (+t.m[1][0] * B4 - t.m[1][1] * B2 + t.m[1][3] * B0) * invDet,
-        (-t.m[1][0] * B3 + t.m[1][1] * B1 - t.m[1][2] * B0) * invDet,
-        (-t.m[0][1] * B5 + t.m[0][2] * B4 - t.m[0][3] * B3) * invDet,
-        (+t.m[0][0] * B5 - t.m[0][2] * B2 + t.m[0][3] * B1) * invDet,
-        (-t.m[0][0] * B4 + t.m[0][1] * B2 - t.m[0][3] * B0) * invDet,
-        (+t.m[0][0] * B3 - t.m[0][1] * B1 + t.m[0][2] * B0) * invDet,
-        (+t.m[3][1] * A5 - t.m[3][2] * A4 + t.m[3][3] * A3) * invDet,
-        (-t.m[3][0] * A5 + t.m[3][2] * A2 - t.m[3][3] * A1) * invDet,
-        (+t.m[3][0] * A4 - t.m[3][1] * A2 + t.m[3][3] * A0) * invDet,
-        (-t.m[3][0] * A3 + t.m[3][1] * A1 - t.m[3][2] * A0) * invDet,
-        (-t.m[2][1] * A5 + t.m[2][2] * A4 - t.m[2][3] * A3) * invDet,
-        (+t.m[2][0] * A5 - t.m[2][2] * A2 + t.m[2][3] * A1) * invDet,
-        (-t.m[2][0] * A4 + t.m[2][1] * A2 - t.m[2][3] * A0) * invDet,
-        (+t.m[2][0] * A3 - t.m[2][1] * A1 + t.m[2][2] * A0) * invDet,
+        .m = {
+            {
+            (+t.m[1][1] * B5 - t.m[1][2] * B4 + t.m[1][3] * B3) * invDet,
+            (-t.m[1][0] * B5 + t.m[1][2] * B2 - t.m[1][3] * B1) * invDet,
+            (+t.m[1][0] * B4 - t.m[1][1] * B2 + t.m[1][3] * B0) * invDet,
+            (-t.m[1][0] * B3 + t.m[1][1] * B1 - t.m[1][2] * B0) * invDet,
+            },
+            {
+            (-t.m[0][1] * B5 + t.m[0][2] * B4 - t.m[0][3] * B3) * invDet,
+            (+t.m[0][0] * B5 - t.m[0][2] * B2 + t.m[0][3] * B1) * invDet,
+            (-t.m[0][0] * B4 + t.m[0][1] * B2 - t.m[0][3] * B0) * invDet,
+            (+t.m[0][0] * B3 - t.m[0][1] * B1 + t.m[0][2] * B0) * invDet,
+            },
+            {
+            (+t.m[3][1] * A5 - t.m[3][2] * A4 + t.m[3][3] * A3) * invDet,
+            (-t.m[3][0] * A5 + t.m[3][2] * A2 - t.m[3][3] * A1) * invDet,
+            (+t.m[3][0] * A4 - t.m[3][1] * A2 + t.m[3][3] * A0) * invDet,
+            (-t.m[3][0] * A3 + t.m[3][1] * A1 - t.m[3][2] * A0) * invDet,
+            },
+            {
+            (-t.m[2][1] * A5 + t.m[2][2] * A4 - t.m[2][3] * A3) * invDet,
+            (+t.m[2][0] * A5 - t.m[2][2] * A2 + t.m[2][3] * A1) * invDet,
+            (-t.m[2][0] * A4 + t.m[2][1] * A2 - t.m[2][3] * A0) * invDet,
+            (+t.m[2][0] * A3 - t.m[2][1] * A1 + t.m[2][2] * A0) * invDet,
+            },
+            }
         };
     return(m);
     }
