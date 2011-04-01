9448f2731db445a996468ea0df90818cdbdf036f
Jonathan Wight <jwight@mac.com>
2011-03-31 20:04:18 -0700

    Default lights aren't so bright.

--- Source/OpenGL/CLight.m
+++ Source/OpenGL/CLight.m
@@ -20,9 +20,9 @@
 	{
 	if ((self = [super init]) != NULL)
 		{
-        ambientColor = (Color4f){ 1.0, 1.0, 1.0, 1.0 };
-        diffuseColor = (Color4f){ 1.0, 1.0, 1.0, 1.0 };
-        specularColor = (Color4f){ 1.0, 1.0, 1.0, 1.0 };
+        ambientColor = (Color4f){ 0.5, 0.5, 0.5, 1.0 };
+        diffuseColor = (Color4f){ 0.5, 0.5, 0.5, 1.0 };
+        specularColor = (Color4f){ 0.5, 0.5, 0.5, 1.0 };
         position = (Vector4){ 0.0, 0.0, 0.0, 0.0 };
 		}
 	return(self);
